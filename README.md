8047cc4cd04d6400f66dab63370e67256e098ec2
Mark Frost <cheerful.mf@gmail.com>
2019-02-04 00:50:06 +0500

    start step 3

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. Get from vk first/last names, city and bdate of people (get_users_from_vk.py)
 2. Clearing data
+3. Visualizing data
--- src/clear_data.py
+++ src/clear_data.py
@@ -1,6 +1,6 @@
 import pandas
 import numpy as np
-
+from datetime import datetime
 import sys
 
 
@@ -13,23 +13,48 @@ def main(argv):
 
     if argv[0] == 'clear':
         clear(data)
+    elif argv[0] == 'with_full_date':
+        with_full_date(data)
+
+
+def get_data_frame():
+    return(pandas.DataFrame(
+        columns=['first_name', 'last_name', 'city',
+                 'bdate', 'type_1', 'type_2', 'type_3', 'type_4']
+    ))
 
 
 def clear(data):
-    data_frame = pandas.DataFrame(
-        columns=['first_name', 'last_name', 'city',
-                 'bdate', 'type_1', 'type_2', 'type_3', 'type_4']
-    )
+    data_frame = get_data_frame()
 
     for i, row in data.iterrows():
         if row['first_name'] != 'DELETED':
-            print(row)
-            data_frame = data_frame.append(row)
+            data_frame = data_frame.append(row,
+                                           ignore_index=True)
 
     data_frame.to_csv(
         "/Users/markfrost/my_projects/ds/test-ml-nik/data/data1.csv",
         sep=';',
         encoding='utf-8'
+    )
+
+
+def with_full_date(data):
+    data_frame = get_data_frame()
+
+    for i, row in data.iterrows():
+        if str(row['bdate']).count('.') == 2:
+            row['bdate'] = datetime.strptime(
+                str(row['bdate']), '%d.%m.%Y').date()
+            data_frame = data_frame.append(row,
+                                           ignore_index=True)
+
+    data_frame.to_csv(
+        "/Users/markfrost/my_projects/ds/test-ml-nik/data/data_with_full_date.csv",
+        sep=';',
+        encoding='utf-8',
+        columns=['first_name', 'last_name', 'city',
+                 'bdate', 'type_1', 'type_2', 'type_3', 'type_4']
     )
 
 
